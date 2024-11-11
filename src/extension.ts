8089f12e50af6ef1ddb7c374fb9618e8d13162da
nerditation <nerditation@users.noreply.github.com>
2024-11-11 15:42:13 +0800

    add copyright notice

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: (C) 2024 nerditation <nerditation@users.noreply.github.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import * as vscode from 'vscode';
