edfcbdbd6a734342f07357a254dc8a5e0f66b838
nerditation <nerditation@users.noreply.github.com>
2024-11-11 15:34:10 +0800

    experiment with distrobox list

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,29 @@
+import { exec } from 'child_process';
+import { promisify } from 'util';
 import * as vscode from 'vscode';
+
+const exec_async = promisify(exec);
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "proposed-api-sample" is now active!');
 
-	/**
-	 * You can use proposed API here. `vscode.` should start auto complete
-	 * Proposed API as defined in vscode.proposed.<proposalName>.d.ts.
-	 */
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider("distrobox.guests", new DistroboxLister)
+	);
+}
 
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+class DistroboxLister implements vscode.TreeDataProvider<string> {
+	getTreeItem(element: string): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return new vscode.TreeItem(element)
+	}
+	async getChildren(element?: string | undefined): Promise<string[]> {
+		if (element) {
+			return []
+		} else {
+			const { stdout } = await exec_async("flatpak-spawn --host distrobox list");
+			let distrobox_list_output = stdout.split("\n");
+			distrobox_list_output.shift();
+			return distrobox_list_output;
+		}
+	}
 }
