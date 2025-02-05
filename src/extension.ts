ae4715318e8e94e99b40ddd5add0aeca97598045
nerditation <nerditation@users.noreply.github.com>
2025-02-05 09:50:00 +0800

    prototype reuse or launch remote server for guest distro

--- src/extension.ts
+++ src/extension.ts
@@ -9,6 +9,7 @@
 import * as vscode from 'vscode';
 
 import * as dbx from './distrobox';
+import { server_binary_path, system_identifier } from './remote';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "proposed-api-sample" is now active!');
@@ -69,6 +70,77 @@ async function connect_command(name?: string) {
 class DistroboxResolver implements vscode.RemoteAuthorityResolver {
 	async resolve(authority: string, context: vscode.RemoteAuthorityResolverContext): Promise<vscode.ResolvedAuthority> {
 		console.log(`resolving ${authority}`);
-		return new vscode.ResolvedAuthority("localhost", 12345)
+		const [_remote, guest_name_encoded] = authority.split('+', 2);
+		const guest_name = decodeURIComponent(guest_name_encoded);
+		const cmd = dbx.MainCommandBuilder.flatpak_spawn_host();
+		const shell = cmd.enter(guest_name, "bash").spawn({ stdio: ['pipe', 'pipe', 'inherit'] });
+		// TODO: PLACEHOLDER:
+		// probe os and architecture properly
+		shell.stdin?.write(
+			`
+			PORT_FILE=\${XDG_RUNTIME_DIR}/vscodium-reh-${system_identifier('linux', 'x64')}/port
+			if [ -f \$PORT_FILE ]; then
+				cat \$PORT_FILE;
+			else
+				echo NOT RUNNING;
+			fi
+			`
+		);
+		shell.stdin?.end();
+		const output: string = await new Promise((resolve, reject) => {
+			shell.stdout?.on('error', reject);
+			let buffer = "";
+			shell.stdout?.on('data', (chunk) => buffer += new TextDecoder("utf8").decode(chunk));
+			shell.stdout?.on('end', () => resolve(buffer))
+		});
+		const running_port = parseInt(output, 10);
+		if (!isNaN(running_port)) {
+			return new vscode.ResolvedAuthority("localhost", running_port)
+		}
+
+		const shell2 = cmd.enter(guest_name, "bash").spawn({ stdio: ['pipe', 'pipe', 'inherit'] });
+		// TODO: PLACEHOLDER:
+		// probe os and architecture properly
+		shell2.stdin?.write(
+			`
+			RUN_DIR=$XDG_RUNTIME_DIR/vscodium-reh-${system_identifier('linux', 'x64')}
+			LOG_FILE=$RUN_DIR/log
+			PID_FILE=$RUN_DIR/pid
+			PORT_FILE=$RUN_DIR/port
+			SERVER_FILE=$HOME/${server_binary_path('linux', 'x64')}
+			if [[ -f $SERVER_FILE ]]; then
+				nohup $SERVER_FILE --accept-server-license-terms --telemetry-level off --host localhost --port 0 --without-connection-token > $LOG_FILE &
+				echo $! > $PID_FILE
+
+				for i in {1..5}; do
+					LISTENING_ON="$(grep -oP '(?<=Extension host agent listening on )\\d+' $LOG_FILE)"
+					if [[ -n $LISTENING_ON ]]; then
+						break
+					fi
+					sleep 0.5
+				done
+
+				if [[ -n $LISTENING_ON ]]; then
+					echo $LISTENING_ON | tee $PORT_FILE
+				else
+					echo ERROR
+				fi
+			else
+				echo NOT INSTALLED
+			fi
+			`
+		);
+		shell2.stdin?.end();
+		const output2: string = await new Promise((resolve, reject) => {
+			shell2.stdout?.on('error', reject);
+			let buffer = "";
+			shell2.stdout?.on('data', (chunk) => buffer += new TextDecoder("utf8").decode(chunk));
+			shell2.stdout?.on('end', () => resolve(buffer))
+		});
+		const running_port2 = parseInt(output2, 10);
+		if (!isNaN(running_port2)) {
+			return new vscode.ResolvedAuthority("localhost", running_port2)
+		}
+		throw ("todo: download and extract server")
 	}
 }
---
+++ src/remote.ts
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: (C) 2025 nerditation <nerditation@users.noreply.github.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import * as vscode from 'vscode';
+
+// this is marked as deprecated, but the supposed replacement `IProductService`
+// isn't publicly available to extensions anyway
+// see https://github.com/microsoft/vscode/blob/d65fd5ba2e7202cac14316c2935680222e1af9cb/src/typings/vscode-globals-product.d.ts#L13
+declare global {
+	const _VSCODE_PRODUCT_JSON: VSCodiumProductInfo;
+}
+
+// this is just the minimum required to calculate the server download url
+interface VSCodiumProductInfo {
+	quality: "insider" | "stable";
+	commit: string;
+	version: string;
+	release: string;
+	serverApplicationName: string,
+	serverDataFolderName: string,
+	// only available in insider builds, but not in stable releases
+	serverDownloadUrlTemplate?: string
+}
+
+/// substitute variables in the template string
+function fill_template(template: string, env: Record<string, string>): string {
+	return template.replace(/\${(.*?)}/g, (_, v) => env[v]);
+}
+
+const INSIDER_SYSTEM_ID_TEMPLATE = "${os}-${arch}-${version}.${release}-insider";
+const STABLE_SYSTEM_ID_TEMPLATE = "${os}-${arch}-${version}.${release}";
+
+export function system_identifier(os: string, arch: string): string {
+	const info = {
+		version: _VSCODE_PRODUCT_JSON.version.replace('-insider', ''),
+		release: _VSCODE_PRODUCT_JSON.release,
+		os,
+		arch,
+	};
+	switch (_VSCODE_PRODUCT_JSON.quality) {
+		case 'insider': return fill_template(INSIDER_SYSTEM_ID_TEMPLATE, info);
+		case 'stable': return fill_template(STABLE_SYSTEM_ID_TEMPLATE, info);
+	}
+}
+
+// this is hardcoded for now
+const DEFAULT_SERVER_DOWNLOAD_URL_TEMPLATE = 'https://github.com/VSCodium/vscodium/releases/download/${version}.${release}/vscodium-reh-${os}-${arch}-${version}.${release}.tar.gz';
+
+/// return the server download url for the given os and arch
+export function server_download_url(os: string, arch: string): string {
+	const info = {
+		version: _VSCODE_PRODUCT_JSON.version.replace('-insider', ''),
+		release: _VSCODE_PRODUCT_JSON.release,
+		os,
+		arch,
+
+	};
+	const template = _VSCODE_PRODUCT_JSON.serverDownloadUrlTemplate ?? DEFAULT_SERVER_DOWNLOAD_URL_TEMPLATE;
+	return fill_template(template, info)
+}
+
+// NOTE:
+// this is different from other extensions, e.g. `open-remote-ssh` use the path
+// `~/.vscodium-server/bin/${commit_hash}`, which I believe follows the
+// convention of the microsoft extensions.
+// I use different path for several reasons:
+// - I'm only interested in `VSCodium`, which has a `release` number to uniquely
+//   identify the exact release version.
+// - distrobox guests typically share the `$HOME` directory, between each other,
+//   and also with the host.
+//   - this might cause conflicts between different guests, e.g. gnu vs musl
+
+/// return the path where the server binary is extracted
+export function server_extract_path(os: string, arch: string): string {
+	return _VSCODE_PRODUCT_JSON.serverDataFolderName + '/bin/vscodium-reh-' + system_identifier(os, arch);
+}
+
+export function server_binary_path(os: string, arch: string): string {
+	return server_extract_path(os, arch) + '/bin/' + _VSCODE_PRODUCT_JSON.serverApplicationName;
+}
